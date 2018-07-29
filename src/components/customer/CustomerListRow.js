6f3451d3ef55cf77a4308ce59de1b2eef052ef1d
Mark Manalo <marksmanalo@gmail.com>
2018-07-29 12:05:04 -0700

    added first and last name

--- src/components/customer/CustomerListRow.js
+++ src/components/customer/CustomerListRow.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 const CustomerListRow = ( {customer} ) => {
   return (
     <tr>
-      <td><Link to={'/customer/' + customer.id}>{customer.firstName}</Link></td>
+      <td><Link to={'/customer/' + customer.id}>{customer.firstName} {customer.lastName}</Link></td>
       <td>{customer.email}</td>
       <td>{customer.phoneNumber}</td>
       <td>{customer.vehicleOfInterest}</td>
