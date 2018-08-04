27d3a4d45b902b59f632bcb45673a0e9faa41844
mmanalo <marksmanalo@marks-mbp-2.attlocal.net>
2018-08-03 22:37:23 -0700

    more renaming

--- src/actions/customerActions.test.js
+++ src/actions/customerActions.test.js
@@ -1,5 +1,5 @@
 import expect from 'expect';
-import * as courseActions from './courseActions';
+import * as customerActions from './customerActions';
 import * as types from './actionTypes';
 import thunk from 'redux-thunk';
 import nock from 'nock';
@@ -13,20 +13,20 @@ describe('Async Action', () => {
     nock.cleanAll();
   });
 
-  it('should create BEGIN_AJAX_CALL and LOAD_COURSE_SUCCESS when loading courses', (done) => {
+  it('should create BEGIN_AJAX_CALL and LOAD_CUSTOMERS_SUCCESS when loading customers', (done) => {
     // Here's an example call to nock to mock a real api
     // nock('http://example.com/')
-    //  .get('/courses')
-    //  .reply(200, {body: {course: [{ id: 1, firstName: 'Cory', lastName: 'House'}]}});
+    //  .get('/customers')
+    //  .reply(200, {body: {customers: [{ id: 1, firstName: 'Cory', lastName: 'House'}]}});
 
     // but we are using a mock api so we dont need to do the above
     const expectedActions = [
       {type: types.BEGIN_AJAX_CALL},
-      {type: types.LOAD_COURSES_SUCCESS, body: {courses: [{id: 'clean-code', title: 'Clean Code'}]}}
+      {type: types.LOAD_CUSTOMERS_SUCCESS, body: {customers: [{id: 'al-romero', firstName: 'al'}]}}
     ];
 
     const store = mockStore({courses: []}, expectedActions);
-    store.dispatch(courseActions.loadCourses()).then(() => {
+    store.dispatch(customerActions.loadCourses()).then(() => {
       const actions = store.getActions();
       expect(actions[0].type).toEqual(types.BEGIN_AJAX_CALL);
       expect(actions[1].type).toEqual(types.LOAD_COURSES_SUCCESS);
